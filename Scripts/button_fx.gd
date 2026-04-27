ef362dacc698c31be905823f0e8a23cc815ad634
BuscandoBugs-Git <lucascsilva1402@gmail.com>
2026-04-27 15:45:00 -0300

    efeitos de menu inicial funcionando

--- Scripts/button_fx.gd
+++ Scripts/button_fx.gd
@@ -2,28 +2,29 @@ extends Node
 
 class_name ButtonsEffectsModule
 
-var ease_type : Tween.EaseType
-var transit_type : Tween.TransitionType
-var anim_duration : float = 0.07
-var color_trans : float = 0.3
-var scale_amount : Vector2 = Vector2(1.4,1.4)
-var pos2 : Vector2 = Vector2(30,0)
-var color1 : Color = Color(1.0, 1.0, 1.0, 1.0)
-var color2 : Color = Color(0.973, 1.0, 0.0, 1.0)
+@export var ease_type : Tween.EaseType
+@export var transit_type : Tween.TransitionType
+@export var anim_duration : float = 0.07
+@export var color_trans : float = 0.3
+@export var dist : int = 60
+@export var color1 : Color = Color(1.0, 1.0, 1.0, 1.0)
+@export var color2 : Color = Color(0.973, 1.0, 0.0, 1.0)
 @onready var button : Button = get_parent()
 
 var tween : Tween
 
 func _ready() -> void:
+	
+	button.modulate = color1
+	
 	button.mouse_entered.connect(_on_mouse_hovered.bind(true))
 	button.mouse_exited.connect(_on_mouse_hovered.bind(false))
 	
 
 func _on_mouse_hovered(hovered : bool):
 	reset_tween()
-	tween.tween_property(button, "position", pos2 if hovered else Vector2.ZERO, anim_duration)
+	tween.tween_property(button, "offset_left", dist if hovered else 0, anim_duration)
 	tween.tween_property(button, "modulate", color2 if hovered else color1, color_trans)
-	
 	
 func reset_tween():
 	if tween:
