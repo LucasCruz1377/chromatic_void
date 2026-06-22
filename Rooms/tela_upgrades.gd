914dfa1eeb5a6ba0df72f964beeb21fbf4e8b700
BuscandoBugs-Git <lucascsilva1402@gmail.com>
2026-06-22 05:24:07 -0300

    falta o func receber upgrade

--- Rooms/tela_upgrades.gd
+++ Rooms/tela_upgrades.gd
@@ -4,19 +4,20 @@ extends Control
 @onready var container = $ContainerCartas
 @export var qtd_cartas : int = 3
 
+@onready var player = get_tree().get_first_node_in_group("player")
+
 func _ready():
-	var player = get_tree().get_first_node_in_group("player")
 	player.subiuDeNivel.connect(_on_player_subiu_de_nivel)
 	print("player obtido")
 	
 
 func _on_player_subiu_de_nivel():
 	var upgrades = sortear_upgrades(3)
-	mostrar_cartas(qtd_cartas)
+	mostrar_cartas(upgrades)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 func sortear_upgrades(qtd : int) -> Array:
-	var tipos = Upgrades.Tipo.keys()
+	var tipos = Upgrades.Tipo.values()
 	tipos.shuffle()
 	return tipos.slice(0, qtd)
 	
@@ -37,5 +38,6 @@ func mostrar_cartas(upgrades):
 	
 func _on_carta_clicada(tipo):
 	print("escolheu upgrade de:  ", tipo)
+	player.receber_upgrade(tipo)
 	for i in get_children():
 		i.queue_free()
